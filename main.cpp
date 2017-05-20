6060c7885126679a9433eae8adaee37de0174bcc
Maxwell Carlson <bart4282819@gmail.com>
2017-05-19 22:25:01 -0700

    added player ship placing

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
  */
 
 #include <cstdlib>
-
+#include <iostream>
 using namespace std;
 
 /*
@@ -22,27 +22,63 @@ using namespace std;
 class Ocean
 {
 public:
-    char ocean[10][10];
+    char playerOcean[10][10];
+    char aiOcean[10][10];
     
     Ocean(){
         for(int i= 0; i < 10; i++){
             for(int j = 0; j < 10; j++){
-                ocean[i][j] = '~';
+                playerOcean[i][j] = '~';
+            }
+        }
+        for(int i= 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                aiOcean[i][j] = '~';
             }
         }
     }
     
     void playerPlace(){
+        cout << "Place your four battle ships! (battleship length of three)" << endl;
         
+        for(int i = 0; i < 4; i++){
+            cout << "Enter the top left most coordinate of ship Y: 0-9 / X: 0-9 (no spaces) " << endl;
+            cout << "Ex: Y1 X2 = 12"
+            int c, c1;
+            cin >> c;
+            cout << "Enter direction your ship is facing 'N' or 'E' " << endl;
+            char d;
+            cin >> d;
+            c1 = c%10;
+            c /= 10;
+            if(d == 'N'){
+                playerOcean[c][c1] = 'B';
+                playerOcean[c+1][c1] = 'B';
+                playerOcean[c+2][c1] = 'B';
+                
+            } else if (d == 'E'){
+                playerOcean[c][c1] = 'B';
+                playerOcean[c][c1+1] = 'B';
+                playerOcean[c][c1+2] = 'B'; 
+            }
+            
+            printPlayerOcean();
+            
+        }
     }
     
-     printOcean(){
-        
+    void printPlayerOcean(){
+        cout << "  ";
         for(int i = 0; i < 10; i++){
+            cout << i << " ";
+        }
+        cout << endl;
+        for(int i = 0; i < 10; i++){
+            cout << i << " ";
             for(int j = 0; j < 10; j++){
-                cout << ocean[i][j];
+                cout << playerOcean[i][j] << " ";
             }
-            cout << endl;
+            std::cout << endl;
         }
     }
     
@@ -55,9 +91,16 @@ public:
 
 int main(int argc, char** argv) {
     
-    Ocean* newOcean = new Ocean();
     
-    newOcean->printOcean;
+    //Ocean* newOcean = new Ocean();
+    //newOcean->printOcean;
+    
+    Ocean newOcean;
+    
+    newOcean.playerPlace();
+    newOcean.printPlayerOcean();
+    
+    
 
     return 0;
 }
